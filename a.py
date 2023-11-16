b983ef5ae2fb9be485a2729897a7250481415267
EllaYu310 <ellay9@nycstudents.net>
2023-11-16 14:00:39 -0500

    Create a.py

---
+++ a.py
@@ -0,0 +1,10 @@
+class start():
+    def begin():
+        go=input("Are you ready to start? ").capitalize()
+        if 'Yes':
+            print("You may now begin. Your code will be printed below")
+            import codes
+            input("")
+        else:
+            print("Do you wanna start again?")
+    begin()
