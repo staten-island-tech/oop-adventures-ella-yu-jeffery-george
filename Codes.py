1f24689409eb60f1c5e4660045c5dd469722edb7
EllaYu310 <ellay9@nycstudents.net>
2023-11-16 13:35:06 -0500

    Create Codes.py

---
+++ Codes.py
@@ -0,0 +1,4 @@
+import random
+
+code=['The fast monkey', 'The fat dog jumped', 'The lazy man sneezed', 'The dog meowed', 'No test too hard, no grade too low', 'No such thing as a bad quote']
+print(random.choice(code))
