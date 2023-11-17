066fe2e19ee05475243cc0800dc9a6fef57954c8
EllaYu310 <ellay9@nycstudents.net>
2023-11-17 14:00:29 -0500

    Create huh.py

---
+++ huh.py
@@ -0,0 +1,15 @@
+def begin():
+    go=input("Are you ready to start? ").capitalize()
+    if go=='No':
+        print("Do you wanna restart?")
+    else:
+        print("You may now begin. Your code will be printed below")
+        print("LT WKZW WTT NIJX, LT EJIXK WTT PTG.")
+        g=input("").lower()
+        
+    if g== "no test too hard, no grade too low":
+        print("Congrats!")
+    else:
+        print("Try again.")  
+begin()
+
