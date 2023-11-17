51a9b440045895505ecd253a2f3754399d474c27
EllaYu310 <ellay9@nycstudents.net>
2023-11-17 14:00:26 -0500

    Create b.py

---
+++ b.py
@@ -0,0 +1,14 @@
+def begin():
+    go=input("Are you ready to start? ").capitalize()
+    if go=='No':
+        print("Ok")
+    else:
+        print("You may now begin. Your code will be printed below")
+        print("LT WKZW WTT NIJX, LT EJIXK WTT PTG.")
+        g=input("").lower()
+        
+        if g== "no test too hard, no grade too low":
+            print("Congrats!")
+        else:
+            print("Try again.")  
+begin()
