8320eb34fe3996bb292ff4a49fb585b493a81d39
EllaYu310 <ellay9@nycstudents.net>
2023-11-20 13:46:36 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,18 @@
+import random
+global x
 class start():
     def begin():
         go=input("Are you ready to start? ").capitalize()
-        if 'Yes':
-            print("You may now begin. Your code will be printed below")
-            import codes
-            input("")
+        if go=='No':
+            print("Do you wanna restart?")
         else:
-            print("Do you wanna start again?")
+            print("You may now begin. Your code will be printed below")
+            za=['the fast monkey', 'the fat dog jumped', 'the lazy man sneezed', 'the dog meowed', 'no test too hard, no grade too low', 'no such thing as a bad quote']
+            x=print(random.choice(za))
+            g=input("").lower()
+            
+        if x==g:
+            print("Congrats!")
+        else:
+            print("Try again.")  
     begin()
