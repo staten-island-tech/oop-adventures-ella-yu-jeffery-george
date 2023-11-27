5a60d6c1fc56d1e63853a558f0637dff74d953de
JGerg01 <jgeorge8655@gmail.com>
2023-11-27 13:41:41 -0500

    Create new opponent

--- newOpponentStats.py
+++ newOpponentStats.py
@@ -1,10 +1,10 @@
 import math
 import random
-n = random.randint(0,138)*5
+n = random.randint(1,60)*5
 def createNewOpponent(n,d):
-    #n is a random number from 0 to 690 deciding the opponents stats
+    #n is a random number from 0 to 300 deciding the opponents stats
     #d is the difficulty, best from -5 to 5, with 0 being around the player's stats
-    return [n,math.floor(((-10*n/3)+2000+(100*d))/5)*5]
+    return [1.1*n,math.floor(((-10*n/3)+2000+(100*d))/5)*5]
     #first item is the attack, second is the health
 
-print(createNewOpponent(n,0))
+print(createNewOpponent(n,5))
