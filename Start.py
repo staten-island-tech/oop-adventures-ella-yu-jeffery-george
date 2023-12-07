58761e7836eeddbe07dc467b15ec5f0c2a0ac118
EllaYu310 <ellay9@nycstudents.net>
2023-12-07 13:47:48 -0500

    Update Start.py

--- Start.py
+++ Start.py
@@ -12,31 +12,44 @@ def beninging():
 beninging()
 g=input("Enter name:")  
               
-def creation():
+def BASE():
     print("""
+Here are the possible base stats
+
+Set A
 Health= 1000
 Attack= 150
 
+Set B
 Health= 2000
 Attack= 50
-        
+  
+Set C      
 Health= 900
 Attack= 300
-        
+     
+Set D       
 Health= 1500
 Attack= 120
         
 """)
+BASE()
+def creation():
+    basic=input("Enter your chosen set, enter only the letter.").lower()
+    me=base_character(g,[],[])
+    if basic=="a":
+        me.set_add(1000,150)
+    elif basic=="b":
+        me.set_add(2000,50)
+    elif basic=="c":
+        me.set_add(900,300)
+    elif basic=="d":
+        me.set_add(1500,120)
+    else:
+        print("Set Choice not found.")
+creation()
 
-#creation()
-
-me=base_character(g,[],[])
-h=int(input("Enter the health of your choice: "))
-a=int(input("Enter the attack of your choice: "))
-me.set_add(h,a)
-
-
-def creating():
+def KITS():
     print("Here are all possible talents")    
     print("""
 Kit one: health increase by 50% of base attack decrease 20%"
@@ -44,23 +57,24 @@ Kit two: heath decreased by 20% of base, attack increase by 50%
 Kit three: health increase by 200% of base, cannot attack next 2 turns
 Kit four: health decrease by 50% attack increase by 130%
 """)
-skill_set=input("Choose a kit: ").lower
-    
-more=talents([])
-    
-if skill_set=="kit one" or "one":
-    print("Kit one selected")
-    more.gain("Health increase by 50% of base attack decrease 20%", turns)
-elif skill_set=="kit two" or "two":
-    print("Kit two selected")
-    more.gain("Heath decreased by 20% of base, attack increase by 50%", turns)
-elif skill_set=="kit three" or "three":
-    print("Kit three selected")
-    more.gain("Health increase by 100% of base cannot attack next 2 turns", turns)
-else:
-    print("Kit four selected")
-    more.gain("Health decrease by 50% attack increase by 130%", turns)
-#creating()
+KITS()
+
+def creating():
+    more=talents([])
+
+    skill_set=input("Choose a kit number:").lower()   
+
+    if skill_set=="one":
+        more.gain("Health increase by 50% of base attack decrease 20%", turns)
+    elif skill_set=="two":
+        more.gain("Heath decreased by 20% of base, attack increase by 50%", turns)
+    elif skill_set=="three":
+        more.gain("Health increase by 100% of base cannot attack next 2 turns", turns)
+    elif skill_set=="four":
+        more.gain("Health decrease by 50% attack increase by 130%", turns)
+    else:
+        print("Kit number not found.")
+creating()
 
 def adventure_time():
     print("Starting journey.")
@@ -71,15 +85,11 @@ def adventure_time():
         if travel==travel:
             fight=[1,2,3,4,5,6,7,8]
             steps=random.randint(1,10)
-        safe=[9,10]
+            safe=[9,10]
+            
         if steps in fight:
             print("Enemy present")
         #import the opponent class
-        elif steps in safe:
+        else:
             print("Item found")
         
-        else:
-            h=0 
-    
-    
-#Call all functions!
