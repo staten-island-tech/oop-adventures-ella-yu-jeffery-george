eb494a1829c68a6442c3d014ef48459736775f88
EllaYu310 <ellay9@nycstudents.net>
2023-12-20 13:32:38 -0500

    .

--- picky.py
+++ picky.py
@@ -1,12 +1,18 @@
-import random
 class picks():
-    def drops():
-        small_potion=("Small Potion", "Restores 200 hp to user.")
-        medium_potion=("Medium Potion", "Restores 500 hp to user.")
-        large_potion=("Large Potion", "Restores 800 hp to user.")
+    import random
+    def pot():    
+        mini=("Mini Potion: Restores 150 hp to user.")
+        small=("Small Potion: Restores 200 hp to user.")
+        standard=("Standard Potion: Restores 350 hp to user")
+        medium=("Medium Potion: Restores 500 hp to user.")
+        large=("Large Potion: Restores 650 hp to user.")
 
         potions=[]
-        potions.append(small_potion)
-        potions.append(medium_potion)
-        potions.append(large_potion)
-    drops()        
+        potions.append(mini)
+        potions.append(small)
+        potions.append(standard)
+        potions.append(medium)
+        potions.append(large)
+
+        chance= random.choice(potions)
+        print(f"Item found! {chance}")
