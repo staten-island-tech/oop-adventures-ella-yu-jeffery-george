1f99c4534141b4e8a77f3762e6b6505ebfe13e54
EllaYu310 <ellay9@nycstudents.net>
2024-01-02 13:21:53 -0500

    c

--- newOpponentStats.py
+++ newOpponentStats.py
@@ -1,14 +1,9 @@
-import random 
 class opponent():
+    import random
     n = random.randint(1,60)*5
-    def createNewOpponent(self,d,n):
-        self.d=d
-        self.n=n
+    def createNewOpponent(d,n):
     #n is a random number from 0 to 300 deciding the opponents stats
     #d is the difficulty, best from -5 to 5, with 0 being around the player's stats
         return [((-10*n/3)+2000+(100*d))/5//1*5,(1+d/10)*n]
     #first item is the health, second is the attack
     #print(f"Opponent health and attack {createNewOpponent(-5,n)}")
-
-
-
