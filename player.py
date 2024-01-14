7d80e79fc31bf5cb07a46f5730ae04ddb59b2167
EllaYu310 <144480564+EllaYu310@users.noreply.github.com>
2024-01-14 17:29:05 -0500

    .

--- player.py
+++ player.py
@@ -15,43 +15,4 @@ Attack: {attack}
         self.inventory.append(picked)
         print(f"Added to inventory. {picked}")
 
-#leave in picky and see if it works
-    #class picks():
-    #    def drops(self, small,medium,large):
-    #        self.small=small
-    #        self.medium=medium
-    #        self.large=large
-    #        potions={"small": 200, "medium": 500, "large": 800}
-    #        
-    #        picked= random.choice(list(potions.keys()))
-    #       return f"Item found: {picked} potion. Replenishes {potions[picked]} health"
-    #    @staticmethod
-    #    def use_potions(self, user_health):
-    #        hpp=input("Which potion would you like to use? small, medium or large?").lower()
-    #        if hpp=="small":
-    #            user_health+=200
-    #        elif hpp=="medium":
-    #            user_health+=500
-    #        elif hpp==("large"):
-    #            user_health+=800
-    #
-    #        return user_health
-        
-        
-        
-    
-    #def creation():
-    #    global user_health
-    #    global user_attack
-    #    global me
-    #    g=input("Enter your name: ")
-    #    me=base_character(g,[],[])
-    #    sets = {"a": (1000,250), "b":(2000,150), "c":(900,300), "d":(1500,220)}
-    #    print(sets)
-    #    basic=input("Enter your chosen set, enter only the letter.").lower()
-    #    if basic in sets:
-    #        me.set_add(*sets[basic])
-    #        user_health = sets[basic][0]
-    #        user_attack = sets[basic][1]  
-    #    else:
-    #        print("Set Choice not found.")
+
--- test.py
+++
@@ -1,4 +0,0 @@
-#from adventuretime import journey
-global go_e
-go_e=input("Begin game? Yes or no?").lower()
-bn=journey(go_e)
